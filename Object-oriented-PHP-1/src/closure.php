abee6df1d74546296d08a8c88c47b25d1a8cdecc
Aulia Eka Putri Aryani <76677130+auliaaepa@users.noreply.github.com>
2021-05-20 10:52:37 +0700

    Create closure.php

---
+++ Object-oriented-PHP-1/src/closure.php
@@ -0,0 +1,29 @@
+<?php
+$print_message1 = function(){
+  echo "Hello!\n";
+};
+$print_message1();
+
+$get_message = function($name){
+  return "Hello, " . $name . "!\n";
+};
+echo $get_message("Steven");
+
+$message = "Hello!";
+$print_message2 = function() use ($message){
+  echo $message;
+  echo "\n";
+};
+$print_message2();
+
+$fruits = ["a" => "Apel", "b" => "Belimbing", "c" => "Cerry"];
+array_walk($fruits, function($value, $key) {
+ echo $key . ". "  . $value . "\n";
+});
+
+$print_fruits = function($value, $key) {
+ echo $key . ". "  . $value . "\n";
+};
+array_walk($fruits, $print_fruits);
+
+?>
