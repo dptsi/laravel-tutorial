30f71f7937cbcdcda3ab23edb9147162fb443563
Fitrah Arie Ramadhan <56785130+Fitrah1812@users.noreply.github.com>
2021-05-25 07:56:57 +0700

    Update FormController.php

--- Laravel-request-validation-and-response/src/Laravel Validation/FormController.php
+++ Laravel-request-validation-and-response/src/Laravel Validation/FormController.php
@@ -1,1 +1,25 @@
-
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+ 
+class FormController extends Controller
+{
+    public function input()
+    {
+        return view('input');
+    }
+ 
+    public function proses(Request $request)
+    {
+        $this->validate($request,[
+           'nama' => 'required|min:5|max:20',
+           'pekerjaan' => 'required',
+           'usia' => 'required|numeric'
+        ]);
+ 		
+        return view('proses',['data' => $request]);
+    }
+}
