5897c6fba1eb2e1179e82c6f0ad0d7d23dff08c7
Fitrah Arie Ramadhan <56785130+Fitrah1812@users.noreply.github.com>
2021-05-26 20:05:36 +0700

    Update web.php

--- Laravel-request-validation-and-response/src/Laravel Response/web.php
+++ Laravel-request-validation-and-response/src/Laravel Response/web.php
@@ -1,1 +1,56 @@
+<?php
 
+use App\Http\Controllers\GuestController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormController;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/input', 'FormController@input');
+// Route::post('/proses', 'FormController@proses');
+
+Route::get('/input', [FormController::class, 'input']);
+Route::post('/proses', [FormController::class, 'proses']);
+
+
+Route::get('/formulir', [GuestController::class, 'input'])->name('input-form-guest');
+Route::post('/proses-form/{id}', [GuestController::class, 'proses'])->name('proses-form-guest');
+
+// basic response
+Route::get('/basic', function(){
+    return 'Hallo ngab, coba basic';
+});
+
+// header
+Route::get('/header', function(){
+    return response('Hallo', 200)->header('Content-Type','text/html');
+});
+
+// Attach Cookie 
+Route::get('/header-cookie', function(){
+    return response('Hallo', 200)
+    ->header('Content-Type','text/html')
+    ->withcookie('name','Fitrah Arie');
+});
+
+// Json respone
+Route::get('/json', function(){
+    return response()->json([
+        'Nama1' => 'Fitrah',
+        'Nama2' => 'Ryan'
+    ]);
+});
