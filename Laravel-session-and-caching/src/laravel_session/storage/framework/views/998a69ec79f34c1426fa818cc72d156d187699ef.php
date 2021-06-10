0cd6921820a621610bfdec3d3c32bf3cdf762b02
vaniameith <61219556+vaniameilani@users.noreply.github.com>
2021-06-10 19:37:34 +0700

    Add files via upload

---
+++ Laravel-session-and-caching/src/laravel_session/storage/framework/views/998a69ec79f34c1426fa818cc72d156d187699ef.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <form method="POST" action="/humans">
+                    <?php echo csrf_field(); ?>
+                    <div class="mb-3">
+                            <label for="no_polisi" class="form-label">Nama</label>
+                            <input type="text" placeholder="Masukkan Nama" name="nama" value="<?php echo e(old('nama')); ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label for="jenis_mesin" class="form-label">Alamat</label>
+                            <input type="text" placeholder="Masukkan Alamat" name="alamat" value="<?php echo e(old('alamat')); ?>">
+                        </div>
+                        <button type="submit" class="btn btn-primary mb-3">Add</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+            
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    </body>
+</html><?php /**PATH C:\xampp\htdocs\pbkk_coba\resources\views/create.blade.php ENDPATH**/ ?>
