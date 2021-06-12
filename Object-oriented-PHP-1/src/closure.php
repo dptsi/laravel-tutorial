dc071e61662f1bb60710142704a80914c77eacca
Aulia Eka Putri Aryani <76677130+auliaaepa@users.noreply.github.com>
2021-06-12 11:16:36 +0700

    Update closure.php

--- Object-oriented-PHP-1/src/closure.php
+++ Object-oriented-PHP-1/src/closure.php
@@ -17,6 +17,7 @@ $print_message2 = function() use ($message){
 $print_message2();
 
 $fruits = ["a" => "Apel", "b" => "Belimbing", "c" => "Cerry"];
+
 array_walk($fruits, function($value, $key) {
  echo $key . ". "  . $value . "\n";
 });
@@ -25,5 +26,10 @@ $print_fruits = function($value, $key) {
  echo $key . ". "  . $value . "\n";
 };
 array_walk($fruits, $print_fruits);
+
+array_walk($fruits, function($value, $key, $fruits) {
+ echo $key . ". "  . $value . "\n";
+ var_dump($fruits);
+}, $fruits);
 
 ?>
